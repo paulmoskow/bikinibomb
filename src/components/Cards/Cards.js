f023dc5b9fdccc9477088fcfd1cb2ccd40f4de6d
Pavel Moskov <pavel.moskov@gmail.com>
2024-05-31 16:49:17 +0200

    app viewport normalazing

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -4,7 +4,7 @@ function Cards() {
   return (
     <article className='cards__container'>
       <div className='cards__container_landing'>
-        <NavLink to="/" className="cards__container_overlay" >
+        <NavLink to="/bikinibomb" className="cards__container_overlay" >
           <h2 className="cards__container_title">Бестселлеры</h2>
         </NavLink>
       </div>
